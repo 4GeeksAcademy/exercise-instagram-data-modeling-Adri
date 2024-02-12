06d5c1ffef8561a2192aaaadd1522b2351f3b783
AdrianRios25 <144917403+AdrianRios2527@users.noreply.github.com>
2024-02-12 20:34:14 +0000

    harrr

--- src/models.py
+++ src/models.py
@@ -5,30 +5,40 @@ from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
+
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class Follower(Base):
+    __tablename__ = 'follower'
+    user_from_id = Column(Integer, primary_key=True)
+    user_to_id = Column(Integer, primary_key=True)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_name = Column(String(250))
+    first_name = Column(String(250))
+    last_name = Column(String(250))
+    email = Column(String(250))
+
+class Media(Base):
+    __tablename__ = 'media'
+    id = Column(Integer, primary_key=True)
+    type = Column(enum)
+    url = Column(String(250))
+    post_id = Column(intenger(250))
+
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(String(250))
+ 
+    
+
 
     def to_dict(self):
         return {}
 
-## Draw from SQLAlchemy base
 try:
     result = render_er(Base, 'diagram.png')
     print("Success! Check the diagram.png file")
